e13e745dfdc344bb2a1bd54763b9241342521c5f
Flavio Pires <flavioasp77@gmail.com>
2022-02-07 20:06:45 -0300

    Requisito 1 - Funções de validação criadas

--- controllers/middlewares.js
+++ controllers/middlewares.js
@@ -3,9 +3,7 @@ const { User } = require('../models');
 
 const SECRET = process.env.JWT_SECRET;
 
-const createToken = async (req, res) => {
-  const { email } = req.body;
-
+const createToken = async (email) => {
   const user = await User.findOne({ where: { email } });
 
   const jwtconfig = {
@@ -16,9 +14,52 @@ const createToken = async (req, res) => {
   delete user.password;
 
   const token = jwt.sign({ payload: user }, SECRET, jwtconfig);
-  res.status(200).json({ token });
+  return token;
+};
+
+const validateDisplayName = (req, res, next) => {
+  const { displayName } = req.body;
+
+  if (displayName.length < 8) {
+    return res.status(400).json(
+      { message: '"displayName" length must be at least 8 characters long' },
+    );
+  }
+  next();
+};
+
+const validateEmail = (req, res, next) => {
+  const { email } = req.body;
+
+  const regexEmail = /\S+@\S+\.\S+/;
+  const validEmail = regexEmail.test(email);
+
+  if (!email || email === '') {
+    return res.status(400).json({ message: '"email" is required' });
+  }
+
+  if (!validEmail) {
+    return res.status(400).json({ message: '"email" must be a valid email' });
+  }
+  next();
+};
+
+const validatePassword = (req, res, next) => {
+  const { password } = req.body;
+
+  if (password.length !== 6) {
+    return res.status(400).json({ message: '"password" length must be 6 characters long' });
+  }
+
+  if (!password || password === '') {
+    return res.status(400).json({ messate: '"password" is required' });
+  }
+  next();
 };
 
 module.exports = {
   createToken,
+  validateDisplayName,
+  validateEmail,
+  validatePassword,
 };
