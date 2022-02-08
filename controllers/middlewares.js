e8a81fca29a7acd437b78e6f0819ca0965bb4c7e
Flavio Pires <flavioasp77@gmail.com>
2022-02-08 13:56:43 -0300

    Requisito 1 finalizado

--- controllers/middlewares.js
+++ controllers/middlewares.js
@@ -47,12 +47,12 @@ const validateEmail = (req, res, next) => {
 const validatePassword = (req, res, next) => {
   const { password } = req.body;
 
-  if (password.length !== 6) {
-    return res.status(400).json({ message: '"password" length must be 6 characters long' });
+  if (!password || password === '') {
+    return res.status(400).json({ message: '"password" is required' });
   }
 
-  if (!password || password === '') {
-    return res.status(400).json({ messate: '"password" is required' });
+  if (password.length !== 6) {
+    return res.status(400).json({ message: '"password" length must be 6 characters long' });
   }
   next();
 };
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const controller = require('./controllers/Users');
-const { createToken } = require('./controllers/middlewares');
+const middlewares = require('./controllers/middlewares');
 
 const app = express();
 app.use(bodyParser.json());
@@ -9,8 +9,10 @@ app.use(bodyParser.json());
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
 app.post('/user',
-  controller.createUser,
-  createToken);
+  middlewares.validateDisplayName,
+  middlewares.validateEmail,
+  middlewares.validatePassword,
+  controller.createUser);
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
