bd1942967179798900bc4366013bdcc660cc6234
Flavio Pires <flavioasp77@gmail.com>
2022-02-11 20:33:53 -0300

    Requisito 7 - Criada a função findCategoryById na camada service

--- services/Categories.js
+++ services/Categories.js
@@ -10,7 +10,18 @@ const getAllCategories = async () => {
   return allCategories;
 };
 
+const findCategoryById = async (categoryIds) => {
+  const findCategory = await Promise.All(
+    categoryIds.map(async (id) => {
+      await Categories.findByPk(id);
+    }),
+  );
+
+  return findCategory.some((id) => id);  
+};
+
 module.exports = {
   createCategory,
   getAllCategories,
+  findCategoryById,
 };
